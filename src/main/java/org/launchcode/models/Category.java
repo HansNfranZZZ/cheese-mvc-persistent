aed5a7609f1905eb9adb42c7c24b4879b3b90514
Hans Steinert <hansnfranzzz@gmail.com>
2019-09-12 15:31:35 -0500

    optimized imports

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,5 +1,4 @@
 package org.launchcode.models;
-import org.launchcode.models.Cheese;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
